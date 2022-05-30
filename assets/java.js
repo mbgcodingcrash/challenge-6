020022ec7fc6a1715bd71584dcc7900a607321ed
mbgcodingcrash <re4u2c@gmail.com>
2022-05-29 22:54:54 -0400

    made the webpage look how i want

---
+++ assets/java.js
@@ -0,0 +1,21 @@
+// fetch api data
+//var searchButtonEl = getElementbyId("submit-button");
+
+const fetchCurrentWeatherData = async () => {
+    const apiKey = "cc286f8e96f455b91b7d19b89ed85a1a"
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=36&lon=-115&appid=${apiKey}'
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data)
+}
+
+const fetchHistoricalWeatherData = async () => {
+    var apiKey = "cc286f8e96f455b91b7d19b89ed85a1a"
+}
+
+
+//event listener for input field
+
+//save input and city data to local storage
+
+//display city data on screen
